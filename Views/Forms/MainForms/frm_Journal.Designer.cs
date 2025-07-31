c6e8045b1b2329da4be7a47ed299ff4b2543f395
Odunsoliman <Oduneg@gmail.com>
2025-07-31 14:26:32 +0300

    Auto Backup - 2025-07-31 14:26:31

--- Views/Forms/MainForms/frm_Journal.Designer.cs
+++ Views/Forms/MainForms/frm_Journal.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MizanOriginalSoft.Views.Forms.MainForms
+﻿namespace MizanOriginalSoft .Views .Forms .MainForms 
 {
     partial class frm_Journal
     {
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DGV
+            // 
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGV.Location = new System.Drawing.Point(3, 46);
+            this.DGV.Name = "DGV";
+            this.DGV.RowHeadersWidth = 51;
+            this.DGV.RowTemplate.Height = 26;
+            this.DGV.Size = new System.Drawing.Size(1268, 225);
+            this.DGV.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClose.Location = new System.Drawing.Point(1236, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(29, 31);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "×";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.DGV, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.74803F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.25197F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1274, 274);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.786885F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 97.21311F));
+            this.tableLayoutPanel2.Controls.Add(this.btnClose, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1268, 37);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1227, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "قيد محاسبى";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frm_Journal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frm_Journal";
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1294, 294);
+            this.ControlBox = false;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frm_Journal";
+            this.Padding = new System.Windows.Forms.Padding(10, 0, 10, 20);
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "قيد محاسبي";
+            this.Load += new System.EventHandler(this.frm_Journal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Views/Forms/MainForms/frm_Journal.cs
+++ Views/Forms/MainForms/frm_Journal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MizanOriginalSoft.MainClasses;
+using Signee.DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,145 @@ namespace MizanOriginalSoft.Views.Forms.MainForms
 {
     public partial class frm_Journal : Form
     {
-        public frm_Journal(int gggg,int llll)
+        public frm_Journal(int Bill_No, int InvTypeID)
         {
             InitializeComponent();
+            billid = Bill_No;
+            typeId = InvTypeID;
+            this.Shown += frm_Journal_Shown;
+
+        }
+        int  billid = 0;
+        int typeId = 0;
+
+        private void frm_Journal_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = DBServiecs.DailyFee_GetJournalEntry(billid, typeId);
+            DGV.DataSource = dt;
+            DGVStyl();
+            DGV.ClearSelection();
+        }
+        private void DGVStyl()
+        {
+            // تعطيل التعديل والإضافة والحذف
+            DGV.ReadOnly = true;
+            DGV.AllowUserToAddRows = false;
+            DGV.AllowUserToDeleteRows = false;
+            DGV.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            DGV.MultiSelect = false;
+
+            // إخفاء الأعمدة غير المرغوبة[]
+            if (DGV.Columns.Contains("ID")) DGV.Columns["ID"].Visible = false;
+            if (DGV.Columns.Contains("Acc_ID")) DGV.Columns["Acc_ID"].Visible = false;
+            if (DGV.Columns.Contains("InvTypeID")) DGV.Columns["InvTypeID"].Visible = false;
+            if (DGV.Columns.Contains("Us_Daily")) DGV.Columns["Us_Daily"].Visible = false;
+            if (DGV.Columns.Contains("Bill_No")) DGV.Columns["Bill_No"].Visible = false;
+            //if (DGV.Columns.Contains("")) DGV.Columns["Bill_No"].Visible = false;
+
+            // إخفاء رؤوس الصفوف
+            DGV.RowHeadersVisible = false;
+
+            // تلوين الصفوف بناءً على محتوى Madeen أو Daeen
+            foreach (DataGridViewRow row in DGV.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                decimal madeen = 0;
+                decimal daeen = 0;
+
+                decimal.TryParse(Convert.ToString(row.Cells["Madeen"].Value), out madeen);
+                decimal.TryParse(Convert.ToString(row.Cells["Daeen"].Value), out daeen);
+
+                if (madeen > 0)
+                {
+                    row.DefaultCellStyle.BackColor = Color.FromArgb(230, 255, 240); // أخضر فاتح جدًا
+                }
+                else if (daeen > 0)
+                {
+                    row.DefaultCellStyle.BackColor = Color.FromArgb(240, 240, 255); // أزرق فاتح جدًا
+                }
+
+                // إخفاء الأصفار بتغيير لون الخط إلى لون الخلفية
+                if (madeen == 0)
+                {
+                    row.Cells["Madeen"].Style.ForeColor = row.DefaultCellStyle.BackColor;
+                }
+
+                if (daeen == 0)
+                {
+                    row.Cells["Daeen"].Style.ForeColor = row.DefaultCellStyle.BackColor;
+                }
+            }
+            // تعيين الخط العام (Times New Roman + Bold) لكل الخلايا
+            Font generalFont = new Font("Times New Roman", 14, FontStyle.Bold);
+            DGV.DefaultCellStyle.Font = generalFont;
+
+            // تعيين نفس الخط + اللون الأزرق لرؤوس الأعمدة
+            DGV.ColumnHeadersDefaultCellStyle.Font = generalFont;
+            DGV.ColumnHeadersDefaultCellStyle.ForeColor = Color.Blue;
+
+            // زيادة ارتفاع رأس العمود
+            DGV.ColumnHeadersHeight = 60; // يمكنك تعديل الرقم حسب الحاجة
+
+            // تلوين خلفية الرأس (اختياري)
+            DGV.ColumnHeadersDefaultCellStyle.BackColor = Color.LightGray;
+            DGV.EnableHeadersVisualStyles = false;
+
+            // إعدادات توسيط العناوين وتوزيع العرض
+            foreach (DataGridViewColumn col in DGV.Columns)
+            {
+                col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+                switch (col.Name)
+                {
+                    case "Madeen":
+                        col.HeaderText = "مدين";
+                        col.FillWeight = 15; // الوزن 1
+                        break;
+
+                    case "Daeen":
+                        col.HeaderText = "دائن";
+                        col.FillWeight = 15; // الوزن 1
+                        break;
+
+                    case "AccName":
+                        col.HeaderText = "اسم الحساب";
+                        col.FillWeight = 20; // الوزن 2
+                        break;
+
+                    case "Bill_Code":
+                        col.HeaderText = "كود السند";
+                        col.FillWeight = 20; // الوزن 2
+                        break;
+
+                    case "dateRDaily":
+                        col.HeaderText = "تاريخ القيد";
+                        col.FillWeight = 20; // الوزن 2
+                        break;
+
+                    case "noteDaily":
+                        col.HeaderText = "ملاحظات";
+                        col.FillWeight = 30; // الوزن 3
+                        break;
+                }
+            }
+
+            // إلغاء تحديد أي صف عند التحميل
+            DGV.ClearSelection();
+
+            // (افتراضيًا يمكن تحديد الصفوف بالماوس طالما ReadOnly=true و MultiSelect=false)
+        }
+
+        private void frm_Journal_Shown(object sender, EventArgs e)
+        {
+            DGV.ClearSelection();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
