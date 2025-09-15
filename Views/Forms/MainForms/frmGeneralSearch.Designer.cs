e8b0ba028d77d6ebc01c836405de1421f101e821
Odunsoliman <Oduneg@gmail.com>
2025-09-15 09:50:09 +0300

    Auto Backup - 2025-09-15 09:50:09

--- Views/Forms/MainForms/frmGeneralSearch.Designer.cs
+++ Views/Forms/MainForms/frmGeneralSearch.Designer.cs
@@ -30,14 +30,23 @@
         {
             tableLayoutPanel1 = new TableLayoutPanel();
             lblTitel = new Label();
+            DGV = new DataGridView();
             tableLayoutPanel2 = new TableLayoutPanel();
             lblcountResulte = new Label();
             txtSearch = new TextBox();
             label1 = new Label();
-            DGV = new DataGridView();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
             tableLayoutPanel1.SuspendLayout();
-            tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)DGV).BeginInit();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -45,16 +54,18 @@
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             tableLayoutPanel1.Controls.Add(lblTitel, 0, 0);
-            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
-            tableLayoutPanel1.Controls.Add(DGV, 0, 2);
+            tableLayoutPanel1.Controls.Add(DGV, 0, 3);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 2);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 0, 1);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.Padding = new Padding(10);
-            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowCount = 4;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 70F));
             tableLayoutPanel1.Size = new Size(800, 450);
             tableLayoutPanel1.TabIndex = 0;
             // 
@@ -69,17 +80,32 @@
             lblTitel.Text = "label2";
             lblTitel.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // DGV
+            // 
+            DGV.AllowUserToAddRows = false;
+            DGV.AllowUserToDeleteRows = false;
+            DGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DGV.Dock = DockStyle.Fill;
+            DGV.Location = new Point(13, 142);
+            DGV.Name = "DGV";
+            DGV.ReadOnly = true;
+            DGV.RowHeadersVisible = false;
+            DGV.Size = new Size(774, 295);
+            DGV.TabIndex = 2;
+            DGV.CellDoubleClick += DGV_CellDoubleClick;
+            // 
             // tableLayoutPanel2
             // 
-            tableLayoutPanel2.ColumnCount = 3;
+            tableLayoutPanel2.ColumnCount = 4;
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45F));
-            tableLayoutPanel2.Controls.Add(lblcountResulte, 2, 0);
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
+            tableLayoutPanel2.Controls.Add(lblcountResulte, 3, 0);
             tableLayoutPanel2.Controls.Add(txtSearch, 1, 0);
             tableLayoutPanel2.Controls.Add(label1, 0, 0);
-            tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(13, 56);
+            tableLayoutPanel2.Controls.Add(button1, 2, 0);
+            tableLayoutPanel2.Location = new Point(13, 99);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 1;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
@@ -92,7 +118,7 @@
             lblcountResulte.Dock = DockStyle.Fill;
             lblcountResulte.Location = new Point(3, 0);
             lblcountResulte.Name = "lblcountResulte";
-            lblcountResulte.Size = new Size(343, 37);
+            lblcountResulte.Size = new Size(305, 37);
             lblcountResulte.TabIndex = 3;
             lblcountResulte.Text = "بحث";
             lblcountResulte.TextAlign = ContentAlignment.MiddleCenter;
@@ -120,19 +146,102 @@
             label1.Text = "بحث";
             label1.TextAlign = ContentAlignment.MiddleRight;
             // 
-            // DGV
+            // tableLayoutPanel3
             // 
-            DGV.AllowUserToAddRows = false;
-            DGV.AllowUserToDeleteRows = false;
-            DGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            DGV.Dock = DockStyle.Fill;
-            DGV.Location = new Point(13, 99);
-            DGV.Name = "DGV";
-            DGV.ReadOnly = true;
-            DGV.RowHeadersVisible = false;
-            DGV.Size = new Size(774, 338);
-            DGV.TabIndex = 2;
-            DGV.CellDoubleClick += DGV_CellDoubleClick;
+            tableLayoutPanel3.ColumnCount = 6;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 13F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 22F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 13F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 22F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 13F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 17F));
+            tableLayoutPanel3.Controls.Add(dateTimePicker1, 1, 0);
+            tableLayoutPanel3.Controls.Add(dateTimePicker2, 3, 0);
+            tableLayoutPanel3.Controls.Add(comboBox1, 5, 0);
+            tableLayoutPanel3.Controls.Add(label2, 0, 0);
+            tableLayoutPanel3.Controls.Add(label3, 2, 0);
+            tableLayoutPanel3.Controls.Add(label4, 4, 0);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(13, 56);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 1;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel3.Size = new Size(774, 37);
+            tableLayoutPanel3.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Dock = DockStyle.Fill;
+            dateTimePicker1.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            dateTimePicker1.Location = new Point(507, 3);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(164, 26);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Dock = DockStyle.Fill;
+            dateTimePicker2.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            dateTimePicker2.Location = new Point(237, 3);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(164, 26);
+            dateTimePicker2.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Dock = DockStyle.Fill;
+            comboBox1.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(3, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(128, 27);
+            comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label2.Location = new Point(677, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 37);
+            label2.TabIndex = 2;
+            label2.Text = "تاريخ البداية : ";
+            label2.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label3.Location = new Point(407, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 37);
+            label3.TabIndex = 2;
+            label3.Text = "تاريخ النهاية : ";
+            label3.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label4.Location = new Point(137, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(94, 37);
+            label4.TabIndex = 2;
+            label4.Text = "محرر الفاتورة : ";
+            label4.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(314, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(32, 31);
+            button1.TabIndex = 4;
+            button1.Text = "x";
+            button1.UseVisualStyleBackColor = true;
             // 
             // frmGeneralSearch
             // 
@@ -145,9 +254,11 @@
             Text = "البحث العام";
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)DGV).EndInit();
             tableLayoutPanel2.ResumeLayout(false);
             tableLayoutPanel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)DGV).EndInit();
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel3.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -160,5 +271,13 @@
         private Label label1;
         private DataGridView DGV;
         private Label lblcountResulte;
+        private TableLayoutPanel tableLayoutPanel3;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
     }
 }
